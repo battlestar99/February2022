6fc6226836ff409397c7b4074a89333f594a6867
“[Razin <anik@agile1tech.com>
2022-03-23 22:12:12 -0400

    adding all file

---
+++ AnotherProject/src/packageTwo/OperatorAndOperation.java
@@ -0,0 +1,16 @@
+package packageTwo;
+
+public class OperatorAndOperation {
+	
+	public static void something() {
+		
+		//10 and 15
+				int numberOne = 10;
+				int numberTwo = 15;
+				int sum = numberOne + numberTwo;
+				System.out.println(sum);
+		
+		
+	}
+
+}
---
+++ AnotherProject/src/packageTwo/TempConverter.java
@@ -0,0 +1,14 @@
+package packageTwo;
+
+public class TempConverter {
+
+	public static void main(String[] args) {
+		double farenhite = 90;
+		
+		double celsius = ((farenhite - 32)*5)/9;
+		System.out.println(celsius);
+		
+		//zakir.hossain@agile1tech.com
+	}
+
+}
